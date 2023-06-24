6e85442a9487fd58334634902da8fc743fe4f30c
Akshata <Akshata@LAPTOP-4CVBJOT7>
2023-06-24 17:34:02 +0530

    Add project files.

---
+++ Employee/EmployeeDetails.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    public class EmployeeDetails
+    {
+        int emp_id;
+        string name;
+        double salary;
+
+        ////public void getdata(int a,string b,double c)
+        ////{
+        ////    emp_id = a;
+        ////    name = b;
+        ////    salary = c;
+        ////}
+            public EmployeeDetails() 
+        { 
+            emp_id = 1;
+            name = "Akshata";
+            salary = 45000;
+        }
+        //public string putdata()
+        //{
+        //    return $" Employee Info {emp_id}/{name}/{salary}";
+        //}
+        public EmployeeDetails(int a,string b,double c)
+        {
+            emp_id = a;
+            name = b;
+            salary = c;
+
+        }
+        public string printdata()
+        {
+            return $"  emp_id - {emp_id}\n  Name - {name} \n   Salary - {salary}";
+        }
+
+    }
+}
---
+++ Employee/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //EmployeeDetails employeeDetails = new EmployeeDetails();
+            //employeeDetails.getdata(101, "Akshata", 45000);
+            //    String show = employeeDetails.putdata();
+            //Console.WriteLine(show);
+            EmployeeDetails details = new EmployeeDetails();
+            Console.WriteLine(details.printdata());
+            EmployeeDetails details1 = new EmployeeDetails(101,"Anjali",50000);
+            Console.WriteLine(details1.printdata());
+        }
+    }
+}
